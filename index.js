00d05aef266b12fa010dbb13c237fe55dfe76cf4
user.name: "Kashif Imran <user.email: "itz.real.kashif@gmail.com>
2024-06-24 03:51:26 +0500

    added the dynamic ID redirect

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./routes/url.js");
 const { connectToMongoDB } = require("./connection.js");
-
+const URL = require("./models/url.js")
 const app = express();
 const PORT = 8000;
 
@@ -10,5 +10,24 @@ connectToMongoDB("mongodb://127.0.0.1:27017/2url-shortener")
 
 app.use(express.json());  // Add this line
 app.use('/url', router);
+
+app.get('/:shortID', async (req, res) => {
+    const shortID = req.params.shortID;
+    try {
+        const result = await URL.findOneAndUpdate(
+            { shortID: shortID },
+            { $push: { visitedHistory: Date.now() } },
+            { new: true }
+        );
+        if (result) {
+            res.status(200).send(result);
+        } else {
+            res.status(404).send({ message: 'URL not found' });
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'Server error', error: error.message });
+    }
+});
+
 
 app.listen(PORT, () => console.log(`The Server is running on PORT ${PORT}`));
