5545d73ee304f8efc7e9ed1095e5148f2bcc3c91
user.name: "Kashif Imran <user.email: "itz.real.kashif@gmail.com>
2024-06-23 18:39:10 +0500

    created the controller

--- controllers/url.js
+++ controllers/url.js
@@ -1,6 +1,19 @@
 const shortid = require('shortid');
-
+const URL = require("../models/url.js")
 
 async function GenerateNewUrl (req, res) {
-    console.log(shortid.generate());
+    const body = req.body;
+    if (!body.url) {
+        return res.status(400).json({error: 'url is required'})
+    }
+    const shortID = shortID(8);
+    await URL.create({
+        shortID: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    })
+
+ return   res.json({id: shortID});
 }
+
+module.exports = GenerateNewUrl;
