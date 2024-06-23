2174512fb96e9f5338d6e3bf1ba97c322f9f9e60
user.name: "Kashif Imran <user.email: "itz.real.kashif@gmail.com>
2024-06-24 01:54:57 +0500

    used my urlRouter in index.js file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const urlRouter = require("./routes/url.js")
+
 const app = express();
 
 const PORT = 8000;
 
-
+app.use('/url', urlRouter)
 
 app.listen(PORT, () => console.log(`The Server is running on PORT ${PORT}`))
