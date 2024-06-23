43a685518934fca87440b980f3c6fdefdb5dc634
user.name: "Kashif Imran <user.email: "itz.real.kashif@gmail.com>
2024-06-24 02:06:45 +0500

    Did the connection of MongoDB in the Index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const urlRouter = require("./routes/url.js")
 
+const {connectToMongoDB} = require("./connection.js")
+
 const app = express();
 
 const PORT = 8000;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/2url-shortener")
+.then( () => console.log("Connected to MongoDB"))
 
 app.use('/url', urlRouter)
 
